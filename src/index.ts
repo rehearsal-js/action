8346df9bfb58727514785aa72f2605672f8853c3
egorio <justegorio@gmail.com>
2022-09-12 14:16:29 -0700

    Init

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { execaSync } from 'execa';
 import { resolve } from 'path';
 import { rehearsal } from '@rehearsal/cli';
 import { a } from './test';
-import { readdirSync } from 'fs';
 
 export function run(): void {
   const srcDir = getInput('src_dir') || '.';
@@ -15,9 +14,7 @@ export function run(): void {
     console.log('987654321');
     console.log(execaSync('yarn', ['install']).stdout); // OR NPM
 
-    console.log(execaSync('ls', ['-la']).stdout);
-
-    console.log(readdirSync('/assets/photos/'));
+    console.log(execaSync('ls', ['-Rla']).stdout);
 
     console.log(execaSync('git', ['config', 'user.name', gitUserName]).stdout);
     console.log(execaSync('git', ['config', 'user.email', gitUserEmail]).stdout);
